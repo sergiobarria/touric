12ef33101afef4ab44879ad8a9b0b48e4a59e76f
sergiobarria <sbarria.dev@gmail.com>
2023-05-26 18:33:48 -0500

    chore: update config

--- server/config/default.ts
+++ server/config/default.ts
@@ -5,5 +5,6 @@ dotenv.config();
 export default {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    MONGO_URI_PROD: process.env.MONGO_URI_PROD
 };
