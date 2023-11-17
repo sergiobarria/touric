68b0110a839d2b74cdc112036b78a97e2f287e06
sergiobarria <sbarria.dev@gmail.com>
2023-11-17 00:50:37 -0500

    delete old project

--- server/src/middlewares/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './morgan.middleware';
-export * from './validate.middleware';
-export * from './globalError.middleware';
--- server/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './logger';
-export * from './apiQueryFeatures';
-export * from './apiError';
