be263b2888fd5933a148dd45ff522243e5e235f6
sergiobarria <sbarria.dev@gmail.com>
2023-11-30 22:53:28 -0500

    Add joi package for env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+
 import { HealthModule } from './health/health.module';
 import { ToursModule } from './tours/tours.module';
 import { UsersModule } from './users/users.module';
 
+const envVarsSchema = Joi.object({
+    PORT: Joi.number().required(),
+    NODE_ENV: Joi.string().valid('development', 'production').required(),
+    DATABASE_CONNECTION: Joi.string().required(),
+});
+
 @Module({
-    imports: [HealthModule, ToursModule, UsersModule],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env', // default
+            isGlobal: true,
+            validationSchema: envVarsSchema,
+        }),
+        HealthModule,
+        ToursModule,
+        UsersModule,
+    ],
     controllers: [],
     providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    const port = configService.get('PORT');
 
+    // ====== App Config ======
     app.setGlobalPrefix('api');
     app.enableVersioning({
         type: VersioningType.URI,
     });
 
-    await app.listen(3000);
+    await app.listen(port);
 }
 bootstrap();
