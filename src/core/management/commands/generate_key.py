514e78d5ad4424dfd822cbdc131eb2e960391b2b
sergiobarria <sbarria.dev@gmail.com>
2024-01-20 23:38:56 -0500

    add command to generate_key

---
+++ src/core/management/commands/generate_key.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.core.management.utils import get_random_secret_key
+
+
+class Command(BaseCommand):
+    help = "Generates a new secret key"
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS("⇨ Generating new secret key..."))
+        self.stdout.write(get_random_secret_key())
